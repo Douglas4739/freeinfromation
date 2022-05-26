086e650315d9fc29360945fc2ec39ed65b84a124
DouglasEde <briancarter340@gmail.com>
2022-05-25 19:25:31 -0700

    added first 7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf';
-import pdfFile from './Mani.pdf'
+import pdfFile from './Mani.pdf';
+import Mani1 from './public/images/Mani1.jpg';
   
 function App() {
   
   return (
     <div>
-      <Document file={pdfFile}>
-      </Document>
+      <img src={Mani1} alt='page 1'></img>
     </div>
   );
 }
