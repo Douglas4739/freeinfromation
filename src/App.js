32407102064d28a059c348156a47808591416e01
DouglasEde <briancarter340@gmail.com>
2022-05-25 20:38:23 -0700

    added style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { Document, Page } from 'react-pdf';
-import pdfFile from './Mani.pdf';
+import './App.css';
+// import { Document, Page } from 'react-pdf';
+// import pdfFile from './Mani.pdf';
 import Mani1 from './images/Mani1.jpg';
+import Mani2 from './images/Mani2.jpg';
+import Mani3 from './images/Mani3.jpg';
+import Mani4 from './images/Mani4.jpg';
+import Mani5 from './images/Mani5.jpg';
+import Mani6 from './images/Mani6.jpg';
+import Mani7 from './images/Mani7.jpg';
+import Mani8 from './images/Mani8.jpg';
+import Mani9 from './images/Mani9.jpg';
+import Mani10 from './images/Mani10.jpg';
+import Mani11 from './images/Mani11.jpg';
+import Mani12 from './images/Mani12.jpg';
+import Mani13 from './images/Mani13.jpg';
   
 function App() {
   
   return (
-    <div>
+    <>
+    {/* <Document file={pdfFile}>
+      
+    </Document> */}
+    <div className="center">
       <img src={Mani1} alt='page 1'></img>
+      <img src={Mani2} alt='page 1'></img>
+      <img src={Mani3} alt='page 1'></img>
+      <img src={Mani4} alt='page 1'></img>
+      <img src={Mani5} alt='page 1'></img>
+      <img src={Mani6} alt='page 1'></img>
+      <img src={Mani7} alt='page 1'></img>
+      <img src={Mani8} alt='page 1'></img>
+      <img src={Mani9} alt='page 1'></img>
+      <img src={Mani10} alt='page 1'></img>
+      <img src={Mani11} alt='page 1'></img>
+      <img src={Mani12} alt='page 1'></img>
+      <img src={Mani13} alt='page 1'></img>
     </div>
+    </>
   );
 }
   
